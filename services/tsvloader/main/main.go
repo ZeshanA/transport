eb60ae53c416a6c118a1fd2fdb9112bf3dd1e69b
Zeshan Amjad <zeshan@zesh.me>
2019-02-02 22:05:30 +0000

    Add mock endpoint

--- services/tsvloader/main/main.go
+++ services/tsvloader/main/main.go
@@ -4,8 +4,10 @@ import (
 	"encoding/csv"
 	"fmt"
 	"io"
+	"net/http"
 	"os"
 	"path/filepath"
+	"strings"
 	"time"
 
 	"github.com/gocarina/gocsv"
@@ -63,6 +65,15 @@ func loadMTAData(path string) ([]ArrivalEntry, error) {
 	return entries, nil
 }
 
+func sayHello(w http.ResponseWriter, r *http.Request) {
+	message := r.URL.Path
+	message = strings.TrimPrefix(message, "/")
+	message = "Hello " + message
+	w.Write([]byte(message))
+}
 func main() {
-	fmt.Printf("Hello, world!")
+	http.HandleFunc("/", sayHello)
+	if err := http.ListenAndServe(":80", nil); err != nil {
+		panic(err)
+	}
 }
