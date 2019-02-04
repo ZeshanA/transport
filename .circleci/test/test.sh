3f067bc39a5c30f7e62fac8306c8a4d5f0c10468
Zeshan Amjad <zeshan@zesh.me>
2019-02-04 10:12:33 +0000

    Add SERVICE_NAME build arg to `docker build` command in test script

--- .circleci/test/test.sh
+++ .circleci/test/test.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # Set working directory
-cd ../services
+cd ../
 
 # Return non-zero exit code if any commands fail
 set -e
@@ -14,5 +14,5 @@ for SERVICE in ${SERVICES}; do
     # Run `docker build` with:
     # -t (tagName): name of the service
     # -f (fileName): path to the service's Dockerfile
-    docker build -t ${SERVICE} -f ${SERVICE}/Dockerfile ${SERVICE}
+    docker build -t ${SERVICE} -f services/${SERVICE}/Dockerfile --build-arg SERVICE_NAME=${SERVICE} .
 done
