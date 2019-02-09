52e7be0a19b80dabb81b95654f5ecc86df38025d
Zeshan Amjad <zeshan@zesh.me>
2019-02-09 18:53:39 +0000

    Allow storage directory to be determined at runtime

--- services/tsvloader/main/main.go
+++ services/tsvloader/main/main.go
@@ -10,15 +10,20 @@ import (
 func main() {
 	hostID, err := strconv.Atoi(os.Args[1])
 	hostCount, err := strconv.Atoi(os.Args[2])
+	storageDirectory := os.Args[3]
 	if err != nil {
 		log.Fatalf("Failed to convert args to integers: %v\n", os.Args)
 	}
-	fetchAndStoreArchives(hostID, hostCount)
+	err = os.MkdirAll(storageDirectory, os.ModePerm)
+	if err != nil {
+		log.Fatalf("Failed to create storage directory %s due to: %v", storageDirectory, err)
+	}
+	fetchAndStoreArchives(hostID, hostCount, storageDirectory)
 }
 
 // Gets URLs for the mtaArchive date range and concurrently
 // fetches and stores the data from each URL
-func fetchAndStoreArchives(hostID int, hostCount int) {
+func fetchAndStoreArchives(hostID int, hostCount int, storageDirectory string) {
 	// Get URLs
 	URLs := getURLsForDateRange(mtaArchiveStartDate, mtaArchiveEndDate)
 	// Number of URLs each host needs to process
@@ -26,22 +31,22 @@ func fetchAndStoreArchives(hostID int, hostCount int) {
 	// The index of the first URL this host should process (based on its ID)
 	firstTaskIndex := hostID * taskCount
 
-	fmt.Printf("Host ID: %d	Host Count: %d", hostID, hostCount)
+	fmt.Printf("Host ID: %d	Host Count: %d\n", hostID, hostCount)
 	fmt.Printf("First Task Index: %d\n", firstTaskIndex)
 	fmt.Printf("Last Task Index: %d\n", firstTaskIndex+taskCount-1)
 
 	// Process 'taskCount' URLs starting from firstTaskIndex
 	for i := firstTaskIndex; i < firstTaskIndex+taskCount; i++ {
-		fetchAndStore(URLs[i])
+		fetchAndStore(URLs[i], storageDirectory)
 	}
 }
 
 // Fetches and stores the data for a single URL
-func fetchAndStore(URL string) {
-	filename := fetchSingleDay(URL)
-	decompressedFilename := decompressFile(filename)
-	validRows := removeNullRows(decompressedFilename)
+func fetchAndStore(URL string, storageDirectory string) {
+	compressedFile := fetchSingleDay(URL, storageDirectory)
+	decompressedFile := decompressFile(compressedFile)
+	validRows := removeNullRows(decompressedFile)
 	arrivalEntries := unmarshalMTADataBytes(validRows)
-	removeDataFiles(filename, decompressedFilename)
+	removeDataFiles(compressedFile, decompressedFile)
 	store(arrivalEntries)
 }
