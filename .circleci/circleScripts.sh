20b93aa90eed3066be2954b33338a696e071f4b6
Zeshan Amjad <zeshan@zesh.me>
2019-02-10 21:14:57 +0000

    Separate services to be tested from services to be deployed

--- .circleci/circleScripts.sh
+++ .circleci/circleScripts.sh
@@ -7,13 +7,14 @@ cd .circleci
 set -e
 
 # List the directory names of your services here:
-SERVICES=""
+SERVICES_TO_TEST="tsvloader"
+SERVICES_TO_DEPLOY=""
 
 # Run test or deploy script based on command arg
 if [[ $1 == "test" ]]; then
-    ./test/test.sh ${SERVICES}
+    ./test/test.sh ${SERVICES_TO_TEST}
 elif [[ $1 == "deploy" ]]; then
-    ./deploy/deploy.sh ${SERVICES}
+    ./deploy/deploy.sh ${SERVICES_TO_DEPLOY}
 else
     echo "Unrecognised command '${1}': you can run 'test' or 'deploy'"
 fi
