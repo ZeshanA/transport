7b3bc1c8232d31ae67eebfe728a9f35b7fcc3cd8
Zeshan Amjad <zeshan@zesh.me>
2019-02-14 15:42:18 +0000

    Run basic API server in livedataloader

--- services/livedataloader/main/main.go
+++ services/livedataloader/main/main.go
@@ -1,11 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"log"
+	"net/http"
+)
 
 func main() {
-	fmt.Println(getHelloWorld())
+	initialiseServer()
 }
 
-func getHelloWorld() string {
-	return "Hello, world!"
+func initialiseServer() {
+	http.HandleFunc("/api/liveData", liveDataRequestHandler)
+	log.Fatal(http.ListenAndServe(":8001", nil))
+}
+
+func liveDataRequestHandler(w http.ResponseWriter, _ *http.Request) {
+	_, err := w.Write([]byte("Hello, world!"))
+	if err != nil {
+		log.Printf("error occurred in liveDataRequestHandler: %s\n", err)
+	}
 }
