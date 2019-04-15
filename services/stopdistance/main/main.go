87207fb71dc2694a078657c7822006c117564723
Zeshan Amjad <zeshan@zesh.me>
2019-04-15 16:34:31 +0100

    Switch to Functional Optional architecture for bustime.client

--- services/stopdistance/main/main.go
+++ services/stopdistance/main/main.go
@@ -1,11 +1,12 @@
 package main
 
 import (
+	"fmt"
 	"transport/lib/bustime"
 	"transport/lib/iohelper"
 )
 
 func main() {
-	client := bustime.Client{Key: iohelper.GetEnv("MTA_API_KEY")}
+	client := bustime.NewClient(iohelper.GetEnv("MTA_API_KEY"))
 	fmt.Println(client.GetAgencies())
 }
