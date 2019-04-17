410e302940dab72c4b81090cd8969eb9035dc0af
Zeshan Amjad <zeshan@zesh.me>
2019-04-17 01:55:54 +0100

    Adapt GetStops to return BusStop structs with lat lon details

--- lib/bustime/endpoints.go
+++ lib/bustime/endpoints.go
@@ -15,6 +15,12 @@ const (
 	stopsEndpoint    = "stops-for-route"
 )
 
+type BusStop struct {
+	StopID    string
+	Latitude  float64
+	Longitude float64
+}
+
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Agencies
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
@@ -43,34 +49,73 @@ func (client *client) GetRoutes(agencyIDs ...string) []string {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
 // GetStops takes a collection of routeIDs and returns a map of
-// the form: routeID -> []stopID
-func (client *client) GetStops(routeIDs ...string) map[string][]string {
-	routeIDtoStopIDs := map[string][]string{}
+// the form: routeID -> directionID -> []stopID
+func (client *client) GetStops(routeIDs ...string) map[string]map[int][]BusStop {
+	mapOfStops := map[string]map[int][]BusStop{}
 	done := make(chan string)
 	for _, routeID := range routeIDs {
-		go client.getStopsForSingleRoute(routeIDtoStopIDs, routeID, done)
+		go client.populateStopsForRoute(mapOfStops, routeID, done)
 	}
 	// Wait for all go routines to report completion to the 'done' channel
 	for i := 0; i < len(routeIDs); i++ {
 		completedRouteID := <-done
 		log.Printf("Succesfully stored stops for route ID: %s\n", completedRouteID)
 	}
-	return routeIDtoStopIDs
+	return mapOfStops
 }
 
-func (client *client) getStopsForSingleRoute(routeIDtoStopIDs map[string][]string, routeID string, done chan string) {
+func (client *client) populateStopsForRoute(mapOfStops map[string]map[int][]BusStop, routeID string, done chan string) {
 	log.Printf("Fetching stops for route ID: %s\n", routeID)
+	// Initialise inner map for this routeID
+	mapOfStops[routeID] = map[int][]BusStop{}
+	// Construct the URL to fetch data from
 	URLWithKey := fmt.Sprintf(
 		"%s/%s/%s.json?%s&includePolylines=false",
 		client.baseURL, stopsEndpoint, routeID, client.MandatoryParams,
 	)
 	// Fetch JSON response containing stopIDs for current routeID
 	jsonString := network.GetRequestBody(URLWithKey)
-	// Extract stopIDs nested within the response and convert to a []string
-	stopIDs := gjson.Get(jsonString, "data.entry.stopIds").Array()
-	stringStopIDs := jsonhelper.ResultArrayToStringArray(stopIDs)
-	// Store list of stopIDs under given routeID
-	routeIDtoStopIDs[routeID] = stringStopIDs
-	// Write routeID to channel to mark as completed
+	// Get the list of travel directions for this routeID
+	// travelDirections := gjson.Get(jsonString, "data.entry.stopGroupings[0].stopGroups").Array()
+	travelDirections := gjson.Get(jsonString, "data.entry.stopGroupings").Array()[0].Get("stopGroups").Array()
+	// Get a map of stopIDs -> stopDetails (JSON strings which contain all of the lat/lon details for each stopID)
+	stopDetails := getStopDetails(jsonString)
+	// For the current routeID, populate each direction (0 or 1) with BusStop structs
+	for _, direction := range travelDirections {
+		client.populateDirectionWithStops(mapOfStops, stopDetails, routeID, direction)
+	}
+	// Write to channel to mark the current routeID as completed
 	done <- routeID
+}
+
+func (client *client) populateDirectionWithStops(
+	mapOfStops map[string]map[int][]BusStop,
+	stopDetails map[string]gjson.Result,
+	routeID string, direction gjson.Result) {
+	// Convert direction to integer (0 or 1)
+	directionID := int(direction.Get("id").Int())
+	// Extract list of stopIDs from JSON
+	stopIDs := jsonhelper.ResultArrayToStringArray(direction.Get("stopIds").Array())
+	// Initialise the list of BusStop structs under the current routeID and directionID
+	mapOfStops[routeID][directionID] = make([]BusStop, len(stopIDs))
+	// Construct a BusStop struct for each stop and store it in the map
+	for i, stopID := range stopIDs {
+		curStopDetails := stopDetails[stopID]
+		lat, lon := curStopDetails.Get("lat").Float(), curStopDetails.Get("lon").Float()
+		stopStruct := BusStop{StopID: stopID, Latitude: lat, Longitude: lon}
+		mapOfStops[routeID][directionID][i] = stopStruct
+	}
+}
+
+// Constructs a map of stopID -> stopDetails from the JSON.
+// The stop details are in an array in the JSON, converting to a map
+// keyed by stopID allows for O(1) extraction of lat/lon values given a stopID.
+func getStopDetails(jsonString string) map[string]gjson.Result {
+	stopIDsMap := map[string]gjson.Result{}
+	stops := gjson.Get(jsonString, "data.references.stops").Array()
+	for _, stop := range stops {
+		stopID := stop.Get("id").String()
+		stopIDsMap[stopID] = stop
+	}
+	return stopIDsMap
 }
--- lib/bustime/endpoints_test.go
+++ lib/bustime/endpoints_test.go
@@ -91,27 +91,179 @@ func TestClient_GetRoutesWithURLEmpty(t *testing.T) {
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 // Stops
 ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
 func TestClient_GetStops(t *testing.T) {
 	// Create HTTP server to serve the correct JSON response, based on the routeID in the request URL
-	responses := map[string]string{
-		"MTA NYCT_M1": `{"data":{"entry":{"stopIds":["MTA_100001","MTA_100002","MTA_100003"]}}}`,
-		"MTA NYCT_M2": `{"data":{"entry":{"stopIds":["MTA_200001","MTA_200002","MTA_200003"]}}}`,
-	}
-	ts := testhelper.ServeMultiResponseMock(responses)
+	ts := testhelper.ServeMultiResponseMock(stopsResponses)
 	defer ts.Close()
 
 	// Create bustime.client
 	client := bustime.NewClient("TEST", bustime.CustomBaseURLOption(ts.URL))
 
-	expected := map[string][]string{
-		"MTA NYCT_M1": {"MTA_100001", "MTA_100002", "MTA_100003"},
-		"MTA NYCT_M2": {"MTA_200001", "MTA_200002", "MTA_200003"},
-	}
+	expected := stopsExpectedOutput
 	// List of routes to fetch stops for
 	routeIDList := []string{"MTA NYCT_M1", "MTA NYCT_M2"}
 	actual := client.GetStops(routeIDList...)
 
 	if !reflect.DeepEqual(expected, actual) {
-		t.Errorf("bustime.GetStops did not return expected map of routeIDs to stopIDs (expected: %s, received: %s)", expected, actual)
+		t.Errorf("bustime.GetStops did not return expected map of routeIDs to stopIDs (expected: %v, received: %v)", expected, actual)
 	}
+}
+
+var stopsResponses = map[string]string{
+	"MTA NYCT_M1": `
+{
+  "data": {
+    "entry": {
+      "stopGroupings": [
+        {
+          "stopGroups": [
+            {
+              "id": 0,
+              "stopIds": [
+                "MTA_100001",
+                "MTA_100002",
+                "MTA_100003"
+              ]
+            },
+            {
+              "id": 1,
+              "stopIds": [
+                "MTA_100004",
+                "MTA_100005",
+                "MTA_100006"
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    "references": {
+      "stops": [
+        {
+          "id": "MTA_100001",
+          "lat": "40.731",
+          "lon": "-73.990"
+        },
+        {
+          "id": "MTA_100002",
+          "lat": "40.831",
+          "lon": "-73.390"
+        },
+        {
+          "id": "MTA_100003",
+          "lat": "40.131",
+          "lon": "-72.890"
+        },
+        {
+          "id": "MTA_100004",
+          "lat": "49.731",
+          "lon": "-73.990"
+        },
+        {
+          "id": "MTA_100005",
+          "lat": "48.831",
+          "lon": "-72.390"
+        },
+        {
+          "id": "MTA_100006",
+          "lat": "49.131",
+          "lon": "-73.890"
+        }
+      ]
+    }
+  }
+}
+`,
+	"MTA NYCT_M2": `
+{
+  "data": {
+    "entry": {
+      "stopGroupings": [
+        {
+          "stopGroups": [
+            {
+              "id": 0,
+              "stopIds": [
+                "MTA_200001",
+                "MTA_200002",
+                "MTA_200003"
+              ]
+            },
+            {
+              "id": 1,
+              "stopIds": [
+                "MTA_200004",
+                "MTA_200005",
+                "MTA_200006"
+              ]
+            }
+          ]
+        }
+      ]
+    },
+    "references": {
+      "stops": [
+        {
+          "id": "MTA_200001",
+          "lat": "48.931",
+          "lon": "-71.290"
+        },
+        {
+          "id": "MTA_200002",
+          "lat": "43.431",
+          "lon": "-75.790"
+        },
+        {
+          "id": "MTA_200003",
+          "lat": "44.231",
+          "lon": "-71.990"
+        },
+        {
+          "id": "MTA_200004",
+          "lat": "48.931",
+          "lon": "-71.290"
+        },
+        {
+          "id": "MTA_200005",
+          "lat": "43.431",
+          "lon": "-75.790"
+        },
+        {
+          "id": "MTA_200006",
+          "lat": "44.231",
+          "lon": "-71.990"
+        }
+      ]
+    }
+  }
+}
+`,
+}
+
+var stopsExpectedOutput = map[string]map[int][]bustime.BusStop{
+	"MTA NYCT_M1": {
+		0: {
+			bustime.BusStop{StopID: "MTA_100001", Latitude: 40.731, Longitude: -73.990},
+			bustime.BusStop{StopID: "MTA_100002", Latitude: 40.831, Longitude: -73.390},
+			bustime.BusStop{StopID: "MTA_100003", Latitude: 40.131, Longitude: -72.890},
+		},
+		1: {
+			bustime.BusStop{StopID: "MTA_100004", Latitude: 49.731, Longitude: -73.990},
+			bustime.BusStop{StopID: "MTA_100005", Latitude: 48.831, Longitude: -72.390},
+			bustime.BusStop{StopID: "MTA_100006", Latitude: 49.131, Longitude: -73.890},
+		},
+	},
+	"MTA NYCT_M2": {
+		0: {
+			{StopID: "MTA_200001", Latitude: 48.931, Longitude: -71.290},
+			{StopID: "MTA_200002", Latitude: 43.431, Longitude: -75.790},
+			{StopID: "MTA_200003", Latitude: 44.231, Longitude: -71.990},
+		},
+		1: {
+			{StopID: "MTA_200004", Latitude: 48.931, Longitude: -71.290},
+			{StopID: "MTA_200005", Latitude: 43.431, Longitude: -75.790},
+			{StopID: "MTA_200006", Latitude: 44.231, Longitude: -71.990},
+		},
+	},
 }
