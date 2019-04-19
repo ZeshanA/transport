0921a401ed226e228dfb32783017b9ad2f83ec80
Zeshan Amjad <zeshan@zesh.me>
2019-04-19 21:52:27 +0100

    Add support for applying function to date range

--- services/labeller/main/main.go
+++ services/labeller/main/main.go
@@ -1,14 +1,24 @@
 package main
 
 import (
+	"database/sql"
 	"fmt"
 	"labeller/stopdistance"
+	"time"
 	"transport/lib/database"
+	"transport/lib/dates"
 )
 
-var db = database.OpenDBConnection()
+var dbConn = database.OpenDBConnection()
 
 func main() {
-	stopDistances := stopdistance.Get(db)
+	stopDistances := stopdistance.Get(dbConn)
 	fmt.Println(stopDistances)
+}
+
+func ProcessDateRange(db *sql.DB, startDate time.Time, lastDate time.Time) {
+	endDate := lastDate.AddDate(0, 0, 1)
+	for d := startDate; !dates.Equal(d, endDate); d = d.AddDate(0, 0, 1) {
+		fmt.Println(d)
+	}
 }
