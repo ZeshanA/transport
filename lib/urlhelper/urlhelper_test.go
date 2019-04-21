d7c7b99c503b3952e50a0af84aea37db35a0ec3d
Zeshan Amjad <zeshan@zesh.me>
2019-04-21 12:08:43 +0100

    lib.urlhelper: fix non-deterministic test case for BuildQueryString

--- lib/urlhelper/urlhelper_test.go
+++ lib/urlhelper/urlhelper_test.go
@@ -14,15 +14,15 @@ var inputs = []map[string]string{
 	{},
 }
 
-var expectedOutputs = []string{
-	"?key1=val1&key2=val2",
-	"?key1=val1",
-	"?key_%1=val%_1",
-	"",
+var expectedOutputs = [][]string{
+	{"?key1=val1&key2=val2", "?key2=val2&key1=val1"},
+	{"?key1=val1"},
+	{"?key_%1=val%_1"},
+	{""},
 }
 
 func TestBuildQueryString(t *testing.T) {
 	for i, params := range inputs {
-		assert.Equal(t, expectedOutputs[i], urlhelper.BuildQueryString(params))
+		assert.Contains(t, expectedOutputs[i], urlhelper.BuildQueryString(params))
 	}
 }
