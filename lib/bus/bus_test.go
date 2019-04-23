1c6d7202780ea970f76122cb0b2c976e2d3e5d22
Zeshan Amjad <zeshan@zesh.me>
2019-04-23 14:21:06 +0100

    Partition dataset fetched from DB for labeller

---
+++ lib/bus/bus_test.go
@@ -0,0 +1,16 @@
+package bus
+
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
+
+func TestPartitionJourneys(t *testing.T) {
+	expected := map[DirectedRoute][]VehicleJourney{
+		{ExampleVJs[0].LineRef.String, int(ExampleVJs[0].DirectionRef.Int64)}: {ExampleVJs[0]},
+		{ExampleVJs[1].LineRef.String, int(ExampleVJs[1].DirectionRef.Int64)}: {ExampleVJs[1]},
+	}
+	actual := PartitionJourneys([]VehicleJourney{ExampleVJs[0], ExampleVJs[1]})
+	assert.Equal(t, expected, actual)
+}
