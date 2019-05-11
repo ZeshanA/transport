dd9e241bbdef5ddc74fe1eff7428760932b22290
Zeshan Amjad <zeshan@zesh.me>
2019-05-11 19:07:54 +0100

    Correct argument parsing bug in CircleCI test script

--- .circleci/test/test.sh
+++ .circleci/test/test.sh
@@ -6,14 +6,15 @@ cd ../
 # Return non-zero exit code if any commands fail
 set -e
 
-# Extract list of services from args
-SERVICES=$1
+# Extract list of services from args by trimming off the first arg (which is the program name)
+SERVICES=${@:1}
 
 # Build and Test all services
 for SERVICE in ${SERVICES}; do
     # Run `docker build` with:
     # -t (tagName): name of the service
     # -f (fileName): path to the service's Dockerfile
+    echo "TESTING ${SERVICE} FROM [${SERVICES}]"
     docker build -t ${SERVICE} -f services/${SERVICE}/Dockerfile --build-arg SERVICE_NAME=${SERVICE} .
 done
 
--- services/labeller/dockerEntrypoint.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/sh
-DEBUG=$1
-
-if [ "${DEBUG}" = "true" ] ; then
-    echo "Running Delve debugger..." && \
-    /go/bin/dlv --listen=:40000 --headless=true --api-version=2 exec ./executable
-else
-    echo "Running executable..." && ./executable
-fi
