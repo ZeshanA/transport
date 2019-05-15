3dcca395d46d01db47e39812535f1daa40d533ed
Zeshan Amjad <zeshan@zesh.me>
2019-05-15 17:58:35 +0100

    Allow trainer base_path to be determined at runtime

--- services/trainer/lib/files.py
+++ services/trainer/lib/files.py
@@ -2,20 +2,21 @@ import json
 import os
 
 
-def save_json(route_id, filename, object):
+def save_json(route_id, object, base_path, filename):
     """
     Saves the given object to models/{routeID}/{filename} in JSON format, creating intermediary folders
     and overwriting the existing file if necessary.
     :param route_id: the route id currently being calculated
-    :param filename: the name of the file to save the object under
     :param object: a JSON-serialisable object to be saved at models/{route_id}/filename
+    :param base_path: a string path to the parent folder of the models directory
+    :param filename: the name of the file to save the object under
     :return: void
     """
-    dir = "models/{}/".format(route_id)
-    filepath = dir + filename
+    directory = "{}/models/{}/".format(base_path, route_id)
+    filepath = directory + filename
     # Create directory if needed
-    if not os.path.exists(dir):
-        os.makedirs(dir)
+    if not os.path.exists(directory):
+        os.makedirs(directory)
     # Delete existing file if needed
     if os.path.exists(filepath):
         os.remove(filepath)
