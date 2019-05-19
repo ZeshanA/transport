c17ae33830a74429ecd7af6393d143dd14670664
Zeshan Amjad <zeshan@zesh.me>
2019-05-19 23:12:28 +0100

    Add list completion handling to get_route_id endpoint

--- services/trainer/train/server.py
+++ services/trainer/train/server.py
@@ -15,10 +15,18 @@ currently_processing: Dict[str, str] = {}
 
 @app.route('/getRouteID')
 def get_route_id():
+    """
+    Endpoint used by hosts to retrieve a new routeID to process.
+    routeIDs are popped from the list of unprocessed routes and assigned
+    to the requester's hostID in the currently_processing dict.
+    :return: string: a routeID, "Complete" if all routeIDs have been processed, or an error message
+    """
     host_id = request.args.get('hostID')
     if not host_id:
         logging.warning("No hostID provided by request, returning error response")
         return "Please provide a hostID in the query string"
+    if not unprocessed_routes:
+        return "Complete"
     route_id = unprocessed_routes.pop()
     currently_processing[host_id] = route_id
     to_do, total = len(unprocessed_routes), len(all_routes)
@@ -30,6 +38,11 @@ def get_route_id():
 
 @app.route('/completeRouteID', methods=['POST'])
 def complete_route_id():
+    """
+    Endpoint used by hosts to mark the completion of training the model for a routeID
+    and to submit the final performance metrics for saving.
+    :return: string: completion message or error message
+    """
     host_id, route_id = request.args.get('hostID'), request.args.get('routeID')
     if not host_id or not route_id:
         logging.warning("Completion request is missing a hostID or a routeID, returning error response")
