b73f227176e3a67e278fc133e5c856b556d24e1a
Zeshan Amjad <zeshan@zesh.me>
2019-05-19 23:08:03 +0100

    Implement routeID completion endpoint

--- services/trainer/train/server.py
+++ services/trainer/train/server.py
@@ -4,6 +4,7 @@ from typing import List, Dict
 
 from flask import Flask, request
 
+from lib.files import save_json
 from lib.routes import all_routes
 
 app = Flask(__name__)
@@ -16,8 +17,8 @@ currently_processing: Dict[str, str] = {}
 def get_route_id():
     host_id = request.args.get('hostID')
     if not host_id:
-        logging.warning("No host ID provided by request, returning error response")
-        return "Please provide a host ID in the query string"
+        logging.warning("No hostID provided by request, returning error response")
+        return "Please provide a hostID in the query string"
     route_id = unprocessed_routes.pop()
     currently_processing[host_id] = route_id
     to_do, total = len(unprocessed_routes), len(all_routes)
@@ -25,6 +26,18 @@ def get_route_id():
     logging.info("Assigned routeID '%s' to hostID '%s'", route_id, host_id)
     logging.info("%d of %d routes complete (%s%%)", to_do, total, completed_percentage)
     return route_id
+
+
+@app.route('/completeRouteID', methods=['POST'])
+def complete_route_id():
+    host_id, route_id = request.args.get('hostID'), request.args.get('routeID')
+    if not host_id or not route_id:
+        logging.warning("Completion request is missing a hostID or a routeID, returning error response")
+        return "Please provide a hostID and a routeID in the query string"
+    del currently_processing[host_id]
+    model_performance = request.get_json()
+    save_json(route_id, model_performance, '.', 'modelPerformance.json')
+    return "Marked routeID {} as complete and saved the performance info.".format(route_id)
 
 
 if __name__ == '__main__':
