2f69f7cf65ed3bc64ca66bba81b9a8b03f33b8c1
Zeshan Amjad <zeshan@zesh.me>
2019-05-20 01:43:19 +0100

    Upload fully trained models to cloud storage

--- services/trainer/train/client.py
+++ services/trainer/train/client.py
@@ -6,11 +6,13 @@ import socket
 import sys
 
 import requests
+import boto3
 
 from lib.data import get_numpy_datasets, merge_np_tuples
 from lib.logs import init_logging
 from lib.models import create_model, calculate_performance_metrics
 
+# TODO: Change server URL and optimal parameters
 SERVER_URL = "http://127.0.0.1:5000/"
 GET_ROUTE_ID_URL = SERVER_URL + "getRouteID"
 COMPLETE_ROUTE_ID_URL = SERVER_URL + "completeRouteID"
@@ -35,7 +37,10 @@ def main():
         upload_performance_metrics(host_id, route_id, model, test)
         # Save model to disk
         filepath = save_model_to_disk(route_id, model)
-        break
+        # Upload model to object storage
+        upload_model(route_id, filepath)
+        # Print ASCII divider for clarity in console
+        print_separator()
 
 
 def get_host_id():
@@ -94,6 +99,13 @@ def get_route_id(host_id):
 
 
 def get_trained_model(params, training):
+    """
+    Creates a model based on the given parameters, trains it on the provided training data
+    and returns a pointer to the fully trained model.
+    :param params: a dict containing the parameters that the model should be trained using
+    :param training: a tuple of Numpy arrays (training_data, training_labels)
+    :return: a pointer to a trained Tensorflow model
+    """
     logging.info("Starting model training...")
     training_data, training_labels = training
     model = create_model(params['hidden_layer_count'], params['neuron_count'], params['activation_function'])
@@ -103,6 +115,14 @@ def get_trained_model(params, training):
 
 
 def upload_performance_metrics(host_id, route_id, model, test):
+    """
+    Uploads performance metrics for the given model to the server.
+    :param host_id: the unique ID representing the current PC to the server
+    :param route_id: the routeID that the model corresponds to
+    :param model: a trained Tensorflow model for the given routeID
+    :param test: a tuple containing examples not yet seen by the model: (testing_data, testing_labels)
+    :return:
+    """
     metrics = calculate_performance_metrics(route_id, model, test)
     metrics_json = json.dumps(metrics)
     req = requests.post(url=COMPLETE_ROUTE_ID_URL, params={'hostID': host_id, 'routeID': route_id}, json=metrics_json)
@@ -117,11 +137,49 @@ def save_model_to_disk(route_id, model):
     :param model: pointer to a trained Tensorflow model
     :return: the (relative) filepath that the model was saved at
     """
-    directory = 'finalModels/{}/'.format(route_id)
-    filepath = '{}/{}-finalModel.h5'.format(directory, route_id)
+    directory = 'models/{}/'.format(route_id)
+    filepath = '{}/finalModel.h5'.format(directory, route_id)
     os.makedirs(directory, exist_ok=True)
     model.save(filepath)
     return filepath
+
+
+def upload_model(route_id, filepath):
+    """
+    Uploads the .h5 file stored at the given filepath to cloud object storage.
+    :param route_id: string: the routeID that the model corresponds to
+    :param filepath: string: the path to the .h5 file containing the trained Tensorflow model
+    """
+    logging.info("Uploading final model for routeID %s to storage...", route_id)
+    session = boto3.session.Session()
+    key_id, secret = get_storage_details()
+    client = session.client('s3',
+                            region_name='fra1',
+                            endpoint_url='https://fra1.digitaloceanspaces.com',
+                            aws_access_key_id=key_id,
+                            aws_secret_access_key=secret)
+    client.upload_file(filepath, 'mtadata', '{}-finalModel.h5'.format(route_id))
+    logging.info("Successfully uploaded final model for routeID %s to storage...", route_id)
+
+
+def get_storage_details():
+    """
+    Fetches the access keys for cloud object storage from the environment.
+    :return: a tuple of strings (access_key_id, secret_access_key)
+    """
+    key_id = os.environ.get('SPACES_KEY_ID')
+    secret = os.environ.get('SPACES_SECRET_KEY')
+    if not key_id or not secret:
+        logging.critical('SPACES_{KEY_ID/SECRET_KEY} NOT SET')
+        raise KeyError
+    return key_id, secret
+
+
+def print_separator():
+    """
+    Prints an ASCII divider to separate different executions in the console
+    """
+    print("\n\n===================================================================================================\n\n")
 
 
 if __name__ == "__main__":
