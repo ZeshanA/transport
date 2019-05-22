ff59f178e05cd13269da663e441b60527b3e3bd3
Zeshan Amjad <zeshan@zesh.me>
2019-05-22 13:10:36 +0100

    detector: Fetch stops for given routeID

--- services/detector/main/main.go
+++ services/detector/main/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"transport/lib/bustime"
+	"transport/lib/iohelper"
+)
 
 func main() {
-	fmt.Println("Hello, world!")
+	routeID := "MTA NYCT_S78"
+	bt := bustime.NewClient(iohelper.GetEnv("MTA_API_KEY"))
+	stops := bt.GetStops(routeID)
+	fmt.Println(stops)
 }
