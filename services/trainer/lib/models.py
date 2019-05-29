369e8050dee25a4b45f1d463d86380782870c904
Zeshan Amjad <zeshan@zesh.me>
2019-05-29 20:24:02 +0100

    Extract neural network model into separate class

--- services/trainer/lib/models.py
+++ services/trainer/lib/models.py
@@ -1,112 +1,35 @@
 import logging
-import os
+from abc import ABC, abstractmethod
 
-import matplotlib.pyplot as plt
-import pandas as pd
-from tensorflow import keras, feature_column
-from tensorflow.keras import layers
-from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
-
-from lib.data import COL_COUNT
 from lib.files import save_json
 
 
-def create_model(hidden_layer_count, neuron_count, activation_function):
-    """
-    Build function used by SciKit to create a Keras classifier.
-    :param hidden_layer_count: number of intermediary layers in the network (excluding feature layer)
-    :param neuron_count: number of neurons in each layer
-    :param activation_function: the activation function applied by each neuron
-    :return: an untrained Keras model backed by Tensorflow
-    """
-    # Start constructing a sequential model
-    model = keras.Sequential()
-    model.add(layers.Dense(COL_COUNT, input_shape=(COL_COUNT,)))
+class Model(ABC):
+    def __init__(self, route_id, params):
+        self.route_id, self.params = route_id, params
+        self.model, self.history = None, None
 
-    # Add additional hidden layers as needed
-    for i in range(hidden_layer_count - 1):
-        model.add(layers.Dense(neuron_count, activation=activation_function))
+    @abstractmethod
+    def create_model(self):
+        pass
 
-    # Output layer, a single number
-    model.add(layers.Dense(1))
+    @abstractmethod
+    def train(self, training):
+        pass
 
-    # Compile model
-    model.compile(loss='mean_squared_error',
-                  optimizer='adam',
-                  metrics=['mean_absolute_error', 'mean_squared_error'])
+    @abstractmethod
+    def calculate_performance_metrics(self, test):
+        pass
 
-    return model
+    def save_performance_metrics(self, test, base_path):
+        """
+        Evaluates a model using the test data provided and writes the calculated
+        metrics to models/{routeID}/finalPerf.json
+        :param test: a pair of Numpy arrays in the format (testing_data, testing_labels)
+        :param base_path: the base_path under which to save the performance data
+        :return:
+        """
+        metrics = self.calculate_performance_metrics(test)
+        save_json(self.route_id, metrics, base_path, "finalPerf.json")
+        logging.info("Successfully saved model performance metrics for routeID %s...", route_id)
 
-
-# Returns a list of tensorflow feature columns, converting text_cols into
-# categorical/embedded columns as needed
-def get_feature_columns(numeric_cols, text_cols):
-    feature_cols = []
-    for col in numeric_cols:
-        feature_cols.append(feature_column.numeric_column(col))
-    for col in text_cols:
-        cat_col = feature_column.categorical_column_with_hash_bucket(col, 10000)
-        emb_col = feature_column.embedding_column(cat_col, 8)
-        feature_cols.append(emb_col)
-    # TODO: maybe remove this line
-    print(list(map(lambda x: x.key if hasattr(x, 'key') else x.categorical_column.key, feature_cols)))
-    return feature_cols
-
-
-# Creates a folder for this route_id and returns a keras callback that can
-# be passed to model.fit to save the model to the folder after every epoch
-def get_checkpoint_callback(route_id):
-    os.makedirs("models/{}".format(route_id))
-    checkpoint_path = "models/{}/cp.ckpt".format(route_id)
-    cp_callback = keras.callbacks.ModelCheckpoint(checkpoint_path)
-    return cp_callback
-
-
-def save_performance_metrics(route_id, model, test, base_path):
-    """
-    Evaluates a model using the test data provided and writes the calculated
-    metrics to models/{routeID}/finalPerf.json
-    :param route_id: the route id currently being calculated
-    :param model: the Keras model to evaluate (any model with support for .predict() should work)
-    :param test: a pair of Numpy arrays in the format (testing_data, testing_labels)
-    :return:
-    """
-    metrics = calculate_performance_metrics(route_id, model, test)
-    save_json(route_id, metrics, base_path, "finalPerf.json")
-    logging.info("Successfully saved model performance metrics for routeID %s...", route_id)
-
-
-def calculate_performance_metrics(route_id, model, test):
-    logging.info("Calculating model performance metrics for routeID %s...", route_id)
-    data, labels = test
-    preds = model.predict(data)
-    return {
-        'route_id': route_id,
-        'mean_absolute_error': mean_absolute_error(labels, preds),
-        'mean_squared_error': mean_squared_error(labels, preds),
-        'r2_score': r2_score(labels, preds)
-    }
-
-
-# Plot train error against validation error using a history.
-# `history` is the result of calling model.fit()
-def plot_history(history):
-    hist = pd.DataFrame(history.history)
-    hist['epoch'] = history.epoch
-
-    plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Abs Error [MPG]')
-    plt.plot(hist['epoch'], hist['mean_absolute_error'], label='Train Error')
-    plt.plot(hist['epoch'], hist['val_mean_absolute_error'], label='Val Error')
-    plt.legend()
-
-    plt.figure()
-    plt.xlabel('Epoch')
-    plt.ylabel('Mean Square Error [$MPG^2$]')
-    plt.plot(hist['epoch'], hist['mean_squared_error'],
-             label='Train Error')
-    plt.plot(hist['epoch'], hist['val_mean_squared_error'],
-             label='Val Error')
-    plt.legend()
-    plt.show()
---
+++ services/trainer/neural_networks/neural_networks.py
@@ -0,0 +1,82 @@
+import logging
+
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.metrics import mean_absolute_error, mean_squared_error, r2_score
+from tensorflow import keras
+from tensorflow.keras import layers
+
+from lib.data import COL_COUNT
+from lib.models import Model
+
+
+class NNModel(Model):
+
+    def __init__(self, route_id):
+        optimal_params = {
+            'hidden_layer_count': 47,
+            'neuron_count': 552,
+            'activation_function': 'relu',
+            'epochs': 18
+        }
+        super().__init__(route_id, optimal_params)
+
+    def create_model(self):
+        # Start constructing a sequential model
+        model = keras.Sequential()
+        model.add(layers.Dense(COL_COUNT, input_shape=(COL_COUNT,)))
+
+        # Add additional hidden layers as needed
+        for i in range(self.params['hidden_layer_count'] - 1):
+            model.add(layers.Dense(self.params['neuron_count'], activation=self.params['activation_function']))
+
+        # Output layer, a single number
+        model.add(layers.Dense(1))
+
+        # Compile model
+        model.compile(loss='mean_squared_error',
+                      optimizer='adam',
+                      metrics=['mean_absolute_error', 'mean_squared_error'])
+
+        self.model = model
+
+    def train(self, training):
+        logging.info("Starting model training...")
+        training_data, training_labels = training
+        self.history = self.model.fit(x=training_data, y=training_labels, epochs=self.params['epochs'])
+        logging.info("Successfully completed model training...")
+
+    def calculate_performance_metrics(self, test):
+        logging.info("Calculating model performance metrics for routeID %s...", route_id)
+        data, labels = test
+        preds = self.model.predict(data)
+        return {
+            'route_id': self.route_id,
+            'mean_absolute_error': mean_absolute_error(labels, preds),
+            'mean_squared_error': mean_squared_error(labels, preds),
+            'r2_score': r2_score(labels, preds)
+        }
+
+    # Plot train error against validation error using a history.
+    # `history` is the result of calling model.fit()
+    def plot_history(self):
+        history = self.history
+        hist = pd.DataFrame(history.history)
+        hist['epoch'] = history.epoch
+
+        plt.figure()
+        plt.xlabel('Epoch')
+        plt.ylabel('Mean Abs Error [MPG]')
+        plt.plot(hist['epoch'], hist['mean_absolute_error'], label='Train Error')
+        plt.plot(hist['epoch'], hist['val_mean_absolute_error'], label='Val Error')
+        plt.legend()
+
+        plt.figure()
+        plt.xlabel('Epoch')
+        plt.ylabel('Mean Square Error [$MPG^2$]')
+        plt.plot(hist['epoch'], hist['mean_squared_error'],
+                 label='Train Error')
+        plt.plot(hist['epoch'], hist['val_mean_squared_error'],
+                 label='Val Error')
+        plt.legend()
+        plt.show()
