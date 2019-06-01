73182c9eb09df8a69a6f571c56f18a558243b68d
Zeshan Amjad <zeshan@zesh.me>
2019-06-01 19:41:47 +0100

    Adapt train script to handle both param search and training tasks

--- services/trainer/train.sh
+++ services/trainer/train.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-export MODEL_TYPE=$1
+export MODEL_TYPE=$2
 export VENV_PATH="/data/za816/trainerEnv"
 export PYTHONPATH="."
 
@@ -9,7 +9,7 @@ source "${VENV_PATH}/bin/activate"
 pip3 install -r requirements.txt
 
 echo "Setup complete, booting train.client to produce ${MODEL_TYPE} models..."
-until python3 "train/client/socket_client.py" ${MODEL_TYPE}; do
+until python3 "train/client/socket_client.py" "$@"; do
         echo "train.client crashed with exit code $?. Restarting..." >&2
         sleep 1
 done
