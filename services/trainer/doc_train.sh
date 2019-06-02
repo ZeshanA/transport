9e18ae00698fc565fa92dd6bad810343f23a3cb4
Zeshan Amjad <zeshan@zesh.me>
2019-06-02 12:43:14 +0100

    Fix environment problems in doc_train script

--- services/trainer/doc_train.sh
+++ services/trainer/doc_train.sh
@@ -4,14 +4,22 @@ export MODEL_TYPE=$2
 export VENV_PATH="/data/za816/.env/transport/trainer"
 export PYTHONPATH="."
 
-virtualenv -p "${HOME}/.pyenv/versions/3.7.3/bin/python3" ${VENV_PATH}
+# Copy pyenv installation over to /data/ to avoid quota
+mkdir -p "/data/za816/.pyenv" && cp -R -n "${HOME}/.pyenv" "/data/za816"
+# Create new virtualenv using the interpreter copied dover
+virtualenv -p "/data/za816/.pyenv/versions/3.7.3/bin/python3" ${VENV_PATH}
+# Activate new virtualenv
 source "${VENV_PATH}/bin/activate"
+# Load in environment variables from .bashrc
 source "${HOME}/.bashrc"
+# Print python3 version
 python3 --version
+# Install dependencies inside virtualenv
 pip3 install -r requirements.txt
 
+# Boot crash-resilient client
 echo "Setup complete, booting train.client to produce ${MODEL_TYPE} models..."
 until python3 "train/client/socket_client.py" "$@"; do
         echo "train.client crashed with exit code $?. Restarting..." >&2
-        sleep 1
+        sleep 10
 done
