6390a5ee98e4ede6783df62ffb33b2fa6255eca6
Zeshan Amjad <zeshan@zesh.me>
2019-06-02 21:51:37 +0100

    Split Detector into evaluation and server mode via CLI

--- services/predictor/lib/model.py
+++ services/predictor/lib/model.py
@@ -1,7 +1,7 @@
 import os
 
 import boto3
-from tensorflow.keras.models import load_model
+import joblib
 
 from lib.data import convert_journey_to_features
 from lib.object_storage import get_storage_details
@@ -10,16 +10,16 @@ from lib.object_storage import get_storage_details
 def predict(model, journey):
     feature_sample = convert_journey_to_features(journey)
     print(feature_sample)
-    prediction = int(model.predict(feature_sample)[0][0])
-    print(model.predict(feature_sample))
+    prediction = int(model.predict(feature_sample)[0])
+    print(prediction)
     return prediction
 
 
 def download_model(route_id):
-    filename = '{}-finalModel.h5'.format(route_id)
+    filename = 'RandomForestModel-{}-finalModel.h5'.format(route_id)
     exists = os.path.isfile(filename.format(route_id))
     if exists:
-        return load_model(filename)
+        return joblib.load(filename)
     # Model doesn't already exist on disk, download from object storage
     session = boto3.session.Session()
     key_id, secret = get_storage_details()
@@ -29,5 +29,5 @@ def download_model(route_id):
                             aws_access_key_id=key_id,
                             aws_secret_access_key=secret)
     with open(filename, 'wb') as f:
-        client.download_fileobj('mtadata', filename, f)
-    return load_model(filename)
+        client.download_fileobj('mtadata3', filename, f)
+    return joblib.load(filename)
