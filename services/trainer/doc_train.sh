1fe2e3a48a4e6467f7b822980385a2de481ce4ed
Zeshan Amjad <zeshan@zesh.me>
2019-06-08 10:42:47 +0100

    Adapt doc_train.sh to use shared virtual env

--- services/trainer/doc_train.sh
+++ services/trainer/doc_train.sh
@@ -1,21 +1,9 @@
 #!/usr/bin/env bash
 
 export MODEL_TYPE=$2
-export VENV_PATH="/data/za816/.env/transport/trainer"
-export PYTHONPATH=".:/data/za816/local"
+export VENV_PATH="/vol/bitbucket/za816/.env/transport/trainer"
+export PYTHONPATH=".:/vol/bitbucket/za816/.local"
 export HOME="/homes/za816"
-
-# Copy pyenv installation over to /data/ to avoid quota
-if [[ ! -f "/data/za816/.pyenv/versions/3.7.3/bin/python3" ]]; then
-    echo "Copying pyenv install to data directory..."
-    mkdir -p "/data/za816/.pyenv" && cp -R -n "${HOME}/.pyenv" "/data/za816"
-    echo "Copying pyenv complete..."
-fi &&
-
-# Create new virtualenv using the interpreter copied over
-if [[ ! -f "${VENV_PATH}/bin/activate" ]]; then
-    virtualenv -p "/data/za816/.pyenv/versions/3.7.3/bin/python3" ${VENV_PATH}
-fi &&
 
 # Activate new virtualenv
 source "${VENV_PATH}/bin/activate" &&
@@ -25,9 +13,6 @@ source "${HOME}/.bashrc" &&
 
 # Print python3 version
 python3 --version &&
-
-# Install dependencies inside virtualenv
-pip3 install --target=/data/za816/local -r requirements.txt &&
 
 # Boot crash-resilient client
 echo "Setup complete, booting train.client to produce ${MODEL_TYPE} models..." &&
