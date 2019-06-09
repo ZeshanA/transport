13cd7aaf6538834ab0d7c69b9490a656d798f3e4
Zeshan Amjad <zeshan@zesh.me>
2019-06-09 18:48:44 +0100

    Add kernel param to SVMModel

--- services/trainer/models/support_vector_machines.py
+++ services/trainer/models/support_vector_machines.py
@@ -1,19 +1,21 @@
 import logging
 
 from sklearn import svm
-from skopt.space import Real
+from skopt.space import Real, Categorical
 
 from lib.models import SKModel
 
 
 class SVMModel(SKModel):
     param_dist = [
-        Real(-3, 2, name='C'),
-        Real(-3, 2, name='epsilon')
+        Categorical(name='kernel', categories=['rbf', 'poly', 'sigmoid']),
+        Real(0.1, 2, name='C'),
+        Real(0.1, 2, name='epsilon')
     ]
-    default_params = {'C': 1, 'epsilon': 0.1}
+    default_params = {'kernel': 'rbf', 'C': 1, 'epsilon': 0.1}
 
+    # noinspection PyPep8Naming
     @staticmethod
-    def create_model(c=default_params['C'], epsilon=default_params['epsilon']):
+    def create_model(kernel=default_params['kernel'], C=default_params['C'], epsilon=default_params['epsilon']):
         logging.info("Creating model...")
-        return svm.SVR(C=c, epsilon=epsilon)
+        return svm.SVR(kernel=kernel, C=C, epsilon=epsilon, gamma='auto')
