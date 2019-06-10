558494cfc45d2c3617e059d8d10a00208ea73313
Zeshan Amjad <zeshan@zesh.me>
2019-06-10 09:22:17 +0100

    Update KNNModel class to include all relevant hyper parameters

--- services/trainer/models/k_nearest_neighbours.py
+++ services/trainer/models/k_nearest_neighbours.py
@@ -1,16 +1,25 @@
 import logging
 
 from sklearn.neighbors import KNeighborsRegressor
-from skopt.space import Integer
+from skopt.space import Integer, Categorical
 
 from lib.models import SKModel
 
 
 class KNNModel(SKModel):
-    param_dist = [Integer(5, 1000, name='n_neighbors')]
-    default_params = {'n_neighbors': 10}
+    param_dist = [
+        Integer(5, 1000, name='n_neighbors'),
+        Categorical(name='weights', categories=['uniform', 'distance']),
+        Categorical(name='metric', categories=['euclidean', 'manhattan', 'chebyshev', 'minkowski']),
+    ]
+    default_params = {
+        'n_neighbors': 10,
+        'weights': 'uniform',
+        'metric': 'minkowski'
+    }
 
     @staticmethod
-    def create_model(n_neighbors=default_params['n_neighbors']):
+    def create_model(n_neighbors=default_params['n_neighbors'], weights=default_params['weights'],
+                     metric=default_params['metric']):
         logging.info("Creating model...")
-        return KNeighborsRegressor(n_neighbors=n_neighbors)
+        return KNeighborsRegressor(n_neighbors=n_neighbors, weights=weights, metric=metric)
