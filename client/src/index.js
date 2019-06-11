f156fd0f31567a7762c148e2d26f456356d7a7c4
Zeshan Amjad <zeshan@zesh.me>
2019-06-11 18:13:53 +0100

    Switch RouteSelector to use Blueprint Suggest component

--- client/src/index.js
+++ client/src/index.js
@@ -2,5 +2,8 @@ import React from "react";
 import ReactDOM from "react-dom";
 import "./index.scss";
 import App from "./App";
+import "../node_modules/@blueprintjs/icons/lib/css/blueprint-icons.css";
+import "../node_modules/@blueprintjs/core/lib/css/blueprint.css";
+import "../node_modules/@blueprintjs/select/lib/css/blueprint-select.css";
 
 ReactDOM.render(<App />, document.getElementById("root"));
