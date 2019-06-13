3f01da79d65898d64c7e3d00872292075b5859ea
Zeshan Amjad <zeshan@zesh.me>
2019-06-13 14:30:44 +0100

    Add support for receiving notifications to front end

--- client/src/App.js
+++ client/src/App.js
@@ -2,30 +2,51 @@ import React, { useEffect, useState } from "react";
 import axios from "axios";
 import "./App.scss";
 import { JourneyForm } from "./components/JourneyForm";
+import { PUSHER_EVENTS, PusherAPI } from "./api/pusher";
+import uniqid from "uniqid";
+import { DepartureAlert } from "./components/DepartureAlert";
 
 function App() {
   const [routes, setRoutes] = useState([]);
+  const [channel, setChannel] = useState(uniqid());
+  const [notification, setNotification] = useState({});
 
   // Fetch routes data if needed
-  useEffect(() => FetchDataEffect(routes, setRoutes), [routes]);
+  useEffect(() => fetchDataEffect(routes, setRoutes), [routes]);
+  useEffect(() => subscribeToPusherChannel(channel, setNotification), [
+    channel
+  ]);
 
   return (
     <div>
+      <DepartureAlert
+        notification={notification}
+        setNotification={setNotification}
+      />
+
       <div className="navbar">
         <h1>
           <i className="icon far fa-bus" />
           DelayGuardian
         </h1>
       </div>
+
       <section className="container">
-        <h2 className="heading">Select a Journey</h2>
-        <JourneyForm routes={routes} />
+        <h2 className="heading">
+          <i className="icon far fa-map-marked" />
+          Subscribe to Departure Notifications
+        </h2>
+        <JourneyForm
+          routes={routes}
+          channel={channel}
+          setChannel={setChannel}
+        />
       </section>
     </div>
   );
 }
 
-const FetchDataEffect = (routes, setRoutes) => {
+const fetchDataEffect = (routes, setRoutes) => {
   if (routes.length !== 0) {
     return;
   }
@@ -33,6 +54,16 @@ const FetchDataEffect = (routes, setRoutes) => {
     const routes = await axios.get("http://127.0.0.1:7891/getStops");
     setRoutes(routes.data);
   })();
+};
+
+const subscribeToPusherChannel = (channel, setNotification) => {
+  PusherAPI.subscribe(
+    channel,
+    PUSHER_EVENTS.DEPARTURE_NOTIFICATION,
+    notification => {
+      setNotification({ ...notification, isOpen: true });
+    }
+  );
 };
 
 export default App;
--- client/src/components/SubscribeButton/index.js
+++ client/src/components/SubscribeButton/index.js
@@ -1,19 +1,54 @@
-import React from "react";
+import React, { useEffect, useState, useRef } from "react";
+import axios from "axios";
+import { Button, Toaster } from "@blueprintjs/core";
+import uniqid from "uniqid";
 import styles from "./index.module.scss";
-import { Button } from "@blueprintjs/core";
+import { serialiseJourney } from "../JourneyForm/data";
 
-export const SubscribeButton = ({ journey, className }) => {
+export const SubscribeButton = props => {
+  const { journey, className, channel, setChannel } = props;
+  const clickedState = useState(false);
+  const setClicked = clickedState[1];
+  const channelState = [channel, setChannel];
+  let toaster = useRef(null);
+  useHandleClick(journey, clickedState, channelState, toaster.current);
   return (
     <Button
-      onClick={handleClick}
+      onClick={() => setClicked(true)}
       className={`${styles.submitButton} ${className}`}
     >
       <i className={`${styles.icon} far fa-bell`} />
       Notify Me!
+      <Toaster ref={toaster} />
     </Button>
   );
 };
 
-const handleClick = () => {
-  alert("Clicked!");
+const useHandleClick = (journey, clickedState, channelState, toaster) => {
+  const [clicked, setClicked] = clickedState;
+  useEffect(() => {
+    if (!clicked) {
+      return;
+    }
+    requestSubscription(journey, toaster, channelState, setClicked);
+  });
+};
+
+async function requestSubscription(journey, toaster, channelState, setClicked) {
+  const [channel, setChannel] = channelState;
+  await axios.post("http://127.0.0.1:7891/subscribe", {
+    ...serialiseJourney(journey),
+    channel: channel
+  });
+  toaster.show(successToast);
+  setClicked(false);
+  setChannel(uniqid());
+}
+
+const successToast = {
+  message:
+    "You're successfully subscribed! We'll let you know which bus to catch.",
+  intent: "success",
+  icon: "endorsed",
+  timeout: 2000
 };
